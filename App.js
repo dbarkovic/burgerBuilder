0cf20b1e819b0ec883483f8a7cc04c5bb7438414
dbarkovic <dinko.barkovic@gmail>
2020-02-08 01:13:40 +0100

    Burger finished 10% - Modal issue

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
 import React from 'react';   
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-
+import Modal from './containers/BurgerBuilder/Modal'
 
 class App extends React.Component {
+  state = {
+    show: false
+  };
+  showModal = e => {
+    this.setState({
+      show: !this.state.show
+    });
+  };
   render () {
     return (
       <div>
         <Layout>
           <BurgerBuilder/>
+
+          <button
+          className="toggle-button"
+          id="centered-toggle-button"
+          onClick={e => {
+            this.showModal(e);
+          }}
+        >
+          {" "}
+          show Modal{" "}
+        </button>
+
+        <Modal onClose={this.showModal} show={this.state.show}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
+          deserunt corrupti, ut fugit magni qui quasi nisi amet repellendus non
+          fuga omnis a sed impedit explicabo accusantium nihil doloremque
+          consequuntur.
+        </Modal>
         </Layout>
+        
       </div>
     );
   }
